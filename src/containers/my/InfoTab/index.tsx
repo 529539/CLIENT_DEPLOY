3394bffac1de90ec477fbc94cac1eb5f507ee5e0
529539 <529539@ewha.ac.kr>
2024-05-29 05:08:48 +0000

    🍱 svg 아이콘 색상 및 크기 수정

---
+++ src/containers/my/InfoTab/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import RoundButton from "@components/RoundButton";
+
+import { Github, Mail } from "@svgs/index";
+import styles from "./index.module.scss";
+import cs from "classnames/bind";
+const cx = cs.bind(styles);
+
+const InfoTab = () => {
+  const { data } = useSession();
+  return (
+    <div className={cx("container")}>
+      <h1>내 정보</h1>
+      <div className={cx("sub-title")}>
+        <h3>계정 정보</h3>
+        <RoundButton
+          text='로그아웃'
+          state={false}
+          className='logout-btn'
+          onClick={() => signOut({ callbackUrl: "/" })}
+        />
+      </div>
+      {data && (
+        <div className={cx("box")}>
+          <div className={cx("img-circle")}>
+            <img
+              src={data.user.image}
+              alt={data.user.name + " profile image"}
+            />
+          </div>
+          <div className={cx("text-container")}>
+            <div>
+              <b>{data.user.name}</b>
+              <i>{data.user.userId}</i>
+              <Github />
+            </div>
+            <div>
+              <Mail />
+              <p>{data.user.email}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfoTab;
